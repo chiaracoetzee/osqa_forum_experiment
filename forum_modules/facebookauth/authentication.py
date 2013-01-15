7624cdbaf9ce602388d5881055bce58839c0ba29
Derrick Coetzee <dcoetzee@eecs.berkeley.edu>
2013-01-15 19:21:36 +0000

    Add edX auth at login, disable all other auths including local auth

--- forum_modules/facebookauth/authentication.py
+++ forum_modules/facebookauth/authentication.py
@@ -21,71 +21,71 @@ except Exception:
         decoder = JSONDecoder()
         return decoder.decode(json.read())
 
-class FacebookAuthConsumer(AuthenticationConsumer):
+# class FacebookAuthConsumer(AuthenticationConsumer):
 
-    def prepare_authentication_request(self, request, redirect_to):
-        args = dict(
-            client_id=settings.FB_API_KEY,
-            redirect_uri="%s%s" % (django_settings.APP_URL, redirect_to),
-            scope="email"
-        )
+#     def prepare_authentication_request(self, request, redirect_to):
+#         args = dict(
+#             client_id=settings.FB_API_KEY,
+#             redirect_uri="%s%s" % (django_settings.APP_URL, redirect_to),
+#             scope="email"
+#         )
 
-        facebook_api_authentication_url = "https://graph.facebook.com/oauth/authorize?" + urlencode(args)
+#         facebook_api_authentication_url = "https://graph.facebook.com/oauth/authorize?" + urlencode(args)
 
-        return facebook_api_authentication_url
+#         return facebook_api_authentication_url
     
-    def process_authentication_request(self, request):
-        try:
-            args = dict(client_id=settings.FB_API_KEY, redirect_uri="%s%s" % (django_settings.APP_URL, request.path))
+#     def process_authentication_request(self, request):
+#         try:
+#             args = dict(client_id=settings.FB_API_KEY, redirect_uri="%s%s" % (django_settings.APP_URL, request.path))
 
-            args["client_secret"] = settings.FB_APP_SECRET  #facebook APP Secret
+#             args["client_secret"] = settings.FB_APP_SECRET  #facebook APP Secret
 
-            args["code"] = request.GET.get("code", None)
-            response = cgi.parse_qs(urlopen("https://graph.facebook.com/oauth/access_token?" + urlencode(args)).read())
-            access_token = response["access_token"][-1]
+#             args["code"] = request.GET.get("code", None)
+#             response = cgi.parse_qs(urlopen("https://graph.facebook.com/oauth/access_token?" + urlencode(args)).read())
+#             access_token = response["access_token"][-1]
 
 
-            user_data = self.get_user_data(access_token)
-            assoc_key = user_data["id"]
+#             user_data = self.get_user_data(access_token)
+#             assoc_key = user_data["id"]
 
-            # Store the access token in cookie
-            request.session["access_token"] = access_token
-            request.session["assoc_key"] = assoc_key
+#             # Store the access token in cookie
+#             request.session["access_token"] = access_token
+#             request.session["assoc_key"] = assoc_key
 
-            # Return the association key
-            return assoc_key
-        except Exception, e:
-            logging.error("Problem during facebook authentication: %s" % e)
-            raise InvalidAuthentication(_("Something wrond happened during Facebook authentication, administrators will be notified"))
+#             # Return the association key
+#             return assoc_key
+#         except Exception, e:
+#             logging.error("Problem during facebook authentication: %s" % e)
+#             raise InvalidAuthentication(_("Something wrond happened during Facebook authentication, administrators will be notified"))
 
-    def get_user_data(self, access_token):
-        profile = load_json(urlopen("https://graph.facebook.com/me?" + urlencode(dict(access_token=access_token))))
+#     def get_user_data(self, access_token):
+#         profile = load_json(urlopen("https://graph.facebook.com/me?" + urlencode(dict(access_token=access_token))))
 
-        name = profile["name"]
+#         name = profile["name"]
 
-        # Check whether the length if the email is greater than 75, if it is -- just replace the email
-        # with a blank string variable, otherwise we're going to have trouble with the Django model.
-        email = smart_unicode(profile['email'])
-        if len(email) > 75:
-            email = ''
+#         # Check whether the length if the email is greater than 75, if it is -- just replace the email
+#         # with a blank string variable, otherwise we're going to have trouble with the Django model.
+#         email = smart_unicode(profile['email'])
+#         if len(email) > 75:
+#             email = ''
 
-        # If the name is longer than 30 characters - leave it blank
-        if len(name) > 30:
-            name = ''
+#         # If the name is longer than 30 characters - leave it blank
+#         if len(name) > 30:
+#             name = ''
 
-        # Return the user data.
-        return {
-            'id' : profile['id'],
-            'username': name,
-            'email': email,
-        }
+#         # Return the user data.
+#         return {
+#             'id' : profile['id'],
+#             'username': name,
+#             'email': email,
+#         }
 
-class FacebookAuthContext(ConsumerTemplateContext):
-    mode = 'BIGICON'
-    type = 'CUSTOM'
-    weight = 100
-    human_name = 'Facebook'
-    code_template = 'modules/facebookauth/button.html'
-    extra_css = []
+# class FacebookAuthContext(ConsumerTemplateContext):
+#     mode = 'BIGICON'
+#     type = 'CUSTOM'
+#     weight = 100
+#     human_name = 'Facebook'
+#     code_template = 'modules/facebookauth/button.html'
+#     extra_css = []
 
-    API_KEY = settings.FB_API_KEY
+#     API_KEY = settings.FB_API_KEY
--- forum_modules/oauthauth/authentication.py
+++ forum_modules/oauthauth/authentication.py
@@ -9,33 +9,33 @@ except ImportError:
 from lib import oauth2
 import settings
 
-class TwitterAuthConsumer(OAuthAbstractAuthConsumer):
-    def __init__(self):
-        OAuthAbstractAuthConsumer.__init__(self,
-                str(settings.TWITTER_CONSUMER_KEY),
-                str(settings.TWITTER_CONSUMER_SECRET),
-                "twitter.com",
-                "https://api.twitter.com/oauth/request_token",
-                "https://api.twitter.com/oauth/access_token",
-                "https://api.twitter.com/oauth/authorize",
-        )
+# class TwitterAuthConsumer(OAuthAbstractAuthConsumer):
+#     def __init__(self):
+#         OAuthAbstractAuthConsumer.__init__(self,
+#                 str(settings.TWITTER_CONSUMER_KEY),
+#                 str(settings.TWITTER_CONSUMER_SECRET),
+#                 "twitter.com",
+#                 "https://api.twitter.com/oauth/request_token",
+#                 "https://api.twitter.com/oauth/access_token",
+#                 "https://api.twitter.com/oauth/authorize",
+#         )
 
-    def get_user_data(self, key):
-        json = self.fetch_data(key, "https://twitter.com/account/verify_credentials.json")
+#     def get_user_data(self, key):
+#         json = self.fetch_data(key, "https://twitter.com/account/verify_credentials.json")
         
-        if 'screen_name' in json:
-            creds = simplejson.loads(json)
+#         if 'screen_name' in json:
+#             creds = simplejson.loads(json)
 
-            return {
-                'username': creds['screen_name']
-            }
+#             return {
+#                 'username': creds['screen_name']
+#             }
         
         
-        return {}
+#         return {}
 
-class TwitterAuthContext(ConsumerTemplateContext):
-    mode = 'BIGICON'
-    type = 'DIRECT'
-    weight = 150
-    human_name = 'Twitter'
-    icon = '/media/images/openid/twitter.png'
+# class TwitterAuthContext(ConsumerTemplateContext):
+#     mode = 'BIGICON'
+#     type = 'DIRECT'
+#     weight = 150
+#     human_name = 'Twitter'
+#     icon = '/media/images/openid/twitter.png'
